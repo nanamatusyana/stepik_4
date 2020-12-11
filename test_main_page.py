3c7e2d802fa988a93c8116ed6a2d8b1dfff8cad1
Nana <nastya.rzevskaya@gmail.com>
2020-12-11 22:29:51 +0300

    exception added

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,16 @@
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+from .pages.main_page import MaimPage
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page= MaimPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MaimPage(browser, link)
+    page.open()
+    page.should_be_login_link()
