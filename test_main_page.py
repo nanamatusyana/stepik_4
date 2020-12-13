c4321067e3f1d6cf840f24d8e4c15b989b61d09b
Nana <nastya.rzevskaya@gmail.com>
2020-12-13 23:09:59 +0300

    switch between main and login page added

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,4 @@
-from .pages.main_page import MaimPage
+from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 def go_to_login_page(browser):
@@ -7,19 +7,22 @@ def go_to_login_page(browser):
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-    page= MaimPage(browser, link)
+    page= MainPage(browser, link)
     page.open()
     page.go_to_login_page()
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-    page = MaimPage(browser, link)
+    page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 def test_guest_can_login(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page=LoginPage(browser, link)
+    link = "http://selenium1py.pythonanywhere.com"
+    page=MainPage(browser, link)
     page.open()
-    page.should_be_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
 
 
 
