73de07d1cb2b870b55d9f61bb3995d429051f4e4
Nana <nastya.rzevskaya@gmail.com>
2020-12-18 23:58:45 +0300

    addeds success message methods

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,9 +6,9 @@ class ProductPage(BasePage):
     def add_to_cart_page(self):
         add_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_CART)
         add_btn.click()
-        self.solve_quiz_and_get_code()
-        alert_name_product=self.browser.find_element(*ProductPageLocators.ALERT_PRODUCT_NAME)
-        return alert_name_product.text
+        #self.solve_quiz_and_get_code()
+        #alert_name_product=self.browser.find_element(*ProductPageLocators.ALERT_PRODUCT_NAME)
+        #return alert_name_product.text
 
     def should_add_to_cart_page(self):
         assert self.is_element_present(*ProductPageLocators.ADD_TO_CART), "Add btn is not presented"
@@ -18,3 +18,11 @@ class ProductPage(BasePage):
     def product_price(self):
         price=self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
         return price.text
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not disappeared"
