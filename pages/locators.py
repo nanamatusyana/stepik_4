309faf9cda4f5b2806212fec545673b4973cedd4
Nana <nastya.rzevskaya@gmail.com>
2020-12-18 23:56:19 +0300

    added BasePageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -10,3 +10,8 @@ class ProductPageLocators:
     PRODUCT_NAME=(By.CSS_SELECTOR, "div.col-sm-6.product_main>h1")
     PRODUCT_PRICE=(By.CSS_SELECTOR, ".price_color")
     ALERT_PRODUCT_NAME=(By.CSS_SELECTOR,"div.alertinner>strong")
+    SUCCESS_MESSAGE=(By.CSS_SELECTOR,".alert-success")
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    
