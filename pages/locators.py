b797b8220b6a56b92306f9fe1519bd2ca5d6de75
Nana <nastya.rzevskaya@gmail.com>
2021-01-08 11:24:43 +0300

    added new locators for login page and basket page

--- pages/locators.py
+++ pages/locators.py
@@ -5,6 +5,13 @@ class MainPageLocators():
 class LoginPageLocators:
     LOGIN_FORM=(By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM=(By.CSS_SELECTOR, "#register_form")
+    USER_EMAIL_FORM = (By.CSS_SELECTOR, "#id_login-username")
+    USER_PASSWORD_FORM= (By.CSS_SELECTOR, "#id_login-password")
+    LOGIN_BTN=(By.CSS_SELECTOR, ".btn-lg")
+    GUEST_EMAIL_FORM = (By.CSS_SELECTOR, "#id_registration-email")
+    GUEST_PASSWORD_FORM1 = (By.CSS_SELECTOR,  "[name='registration-password1']")
+    GUEST_PASSWORD_FORM2 = (By.CSS_SELECTOR, "[name='registration-password2']")
+    REG_BTN = (By.CSS_SELECTOR, "[name='registration_submit']")
 class ProductPageLocators:
     ADD_TO_CART=(By.CSS_SELECTOR, ".btn-add-to-basket")
     PRODUCT_NAME=(By.CSS_SELECTOR, "div.col-sm-6.product_main>h1")
@@ -14,4 +21,8 @@ class ProductPageLocators:
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
-    
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+class BasketPageLocators:
+    VIEW_CART_BTN = (By.CSS_SELECTOR, "div.basket-mini span.btn-group a.btn.btn-default")
+    EMPTY_CART=(By.CSS_SELECTOR, "#content_inner p")
+    ITEMS_IN_CART=(By.CSS_SELECTOR, ".basket-items")
