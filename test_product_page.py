35fbc581eb02344c1e04f762a66a84a0341a67e3
Nana <nastya.rzevskaya@gmail.com>
2021-01-08 11:17:02 +0300

    now all tests without assert

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 from .pages.locators import ProductPageLocators
+from .pages.basket_page import BasketPage
 import time
 import pytest
 
@@ -13,63 +14,71 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.register_new_user(str(time.time()) + "@fakemail.org",str(time.time()))
         page.should_be_authorized_user()
+
     def test_user_cant_see_success_message(self,browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
         page = ProductPage(browser, link)
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self,browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
         page = ProductPage(browser, link)
         page.open()
         page.add_to_cart_page()
-        assert (page.product_name() == page.add_to_cart_page())
         time.sleep(1)
 
-
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-        page = ProductPage(browser, link)
-        page.open()
-        page.add_to_cart_page()
-        assert (page.product_name()==page.add_to_cart_page())
-        time.sleep(1)
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart_page()
+    time.sleep(1)
 
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-        page = ProductPage(browser, link)
-        page.open()
-        page.add_to_cart_page()
-        assert page.is_not_element_present(
-            *ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart_page()
+    page.should_not_be_success_message()
 
 def test_guest_cant_see_success_message(browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-        page = ProductPage(browser, link)
-        page.open()
-        assert page.is_not_element_present(
-            *ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
 
 def test_message_disappeared_after_adding_product_to_basket(browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-        page = ProductPage(browser, link)
-        page.open()
-        page.add_to_cart_page()
-        assert page.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), 'Sucsess messege isnt disappeared'
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart_page()
+    page.should_disappeared()
 
 def test_guest_should_see_login_link_on_product_page(browser):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-        page = ProductPage(browser, link)
-        page.open()
-        page.should_be_login_link()
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-        page = ProductPage(browser, link)
-        page.open()
-        page.go_to_login_page()
-        login_page = LoginPage(browser, browser.current_url)
-        login_page.should_be_login_page()
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page =BasketPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.should_be_empty_basket()
+    page.should_be_empty_basket_massege()
+
 
 
